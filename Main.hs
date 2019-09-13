7272c6363821995b0958f0d1c87108de26125518
BriDLeiva <sophos@live.com>
2019-09-13 16:38:21 -0600

    sucessfully calls the weather API.

--- Main.hs
+++ Main.hs
@@ -4,24 +4,37 @@ module Main where
 
 import Servant
 import Network.Wai.Handler.Warp
+import Network.HTTP.Simple (httpLBS, getResponseStatusCode, parseRequest)
+
 
 type RootEndpoint = Get '[PlainText] MyMSG
 type MyMSG = String
 
 
-defaultMsg :: MyMSG
-defaultMsg = "let's now fetch London's temp!"
+tempMsg :: String -> MyMSG
+tempMsg degrees = "Hello from London, it is " ++ degrees ++"C\n"
 
-server :: Server RootEndpoint
-server = return defaultMsg
+tempServer :: String -> Server RootEndpoint
+tempServer = return . tempMsg
 
 msgAPI :: Proxy RootEndpoint
 msgAPI = Proxy
 
-myApp :: Application
-myApp = serve msgAPI server
+tempApp :: String -> Application
+tempApp = (serve msgAPI) . tempServer
+
+myEndpoint :: String
+myEndpoint = "https://api.openweathermap.org/data/2.5/forecast?id=2643743&APPID=82cf415ef1f8714d6709871fc4147d0a"
 
 main :: IO ()
 main = do 
     putStrLn "Hello, Haskell!"
-    run 8081 myApp
+    req <- parseRequest myEndpoint
+    resp <- httpLBS req
+
+    putStrLn $ show (getResponseStatusCode resp)
+
+    run 8081 (myApp resp)
+
+    where
+        myApp = tempApp . show . getResponseStatusCode
