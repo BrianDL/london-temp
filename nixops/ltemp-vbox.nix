dc25dc159bbde2ad8c2a2939156e29d1ffce2cdf
BriDLeiva <sophos@live.com>
2019-09-15 03:43:04 -0600

    Made the virtual machine a litle more reasonable

--- nixops/ltemp-vbox.nix
+++ nixops/ltemp-vbox.nix
@@ -2,7 +2,7 @@
   webserver =
     { config, pkgs, ... }:
     { deployment.targetEnv = "virtualbox";
-      deployment.virtualbox.memorySize = 4096; # megabytes
-      deployment.virtualbox.vcpu = 3; # number of cpus
+      deployment.virtualbox.memorySize = 2048; # megabytes
+      deployment.virtualbox.vcpu = 2; # number of cpus
     };
 }
